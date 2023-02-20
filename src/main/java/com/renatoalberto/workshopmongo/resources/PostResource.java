651de538fe2120d3abdefd4a96a1ccaff299011c
Renato Farias <santosrenato351@gmail.com>
2023-02-20 12:57:04 -0300

    consultation with various criteria

--- src/main/java/com/renatoalberto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/renatoalberto/workshopmongo/resources/PostResource.java
@@ -1,11 +1,13 @@
 package com.renatoalberto.workshopmongo.resources;
 
 import com.renatoalberto.workshopmongo.domain.Post;
+import com.renatoalberto.workshopmongo.resources.util.URL;
 import com.renatoalberto.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -27,4 +29,18 @@ public class PostResource {
         List<Post> posts = postService.findByTitle(text);
         return ResponseEntity.ok().body(posts);
     }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        //treating values received
+        text = URL.decodeParam(text);
+        Date min = URL.converDate(minDate, new Date(0L));
+        Date max = URL.converDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
--- src/main/java/com/renatoalberto/workshopmongo/services/PostService.java
+++ src/main/java/com/renatoalberto/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.renatoalberto.workshopmongo.services.exception.ObjectNotFoundExceptio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -26,4 +27,9 @@ public class PostService {
         return postRepository.findByTitleContainingIgnoreCase(text);
     }
 
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        //increasing a day on maxDate to pick up everything posted during the day
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
+    }
 }
